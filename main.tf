b2b7402f47ac62d3d80c04b23bda319c8c1db1cd
isourabhkr <112383191+isourabhkr@users.noreply.github.com>
2023-10-10 21:42:34 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = var.project_test
+  region  = "europe-west4"
+  zone    = "europe-west4-b"
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "project_test" {
+  description = "Test project id"
+  type = string
+  default = "pde-test"
+}
